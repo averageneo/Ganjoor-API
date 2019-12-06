086c846448b097d6aaa682497c2ea17a71708dd8
nelliel <arch7nelliel@gmail.com>
2019-12-06 15:38:22 +0330

    utilized as snippet

--- poetID.py
+++ poetID.py
@@ -2,19 +2,19 @@
 
 import sqlite3
 from pprint import pprint
+def peotid():
+    connect = sqlite3.connect('database.sqlite')
+    cursor = connect.cursor()
 
-connect = sqlite3.connect('database.sqlite')
-cursor = connect.cursor()
+    cursor.execute('SELECT * FROM poets')
+    rows = cursor.fetchall()
 
-cursor.execute('SELECT * FROM poets')
-rows = cursor.fetchall()
+    poets = []
+    for row in rows:
+        poet={
+        'Name': row[1],
+        'ID': row[0]
+        }
+        poets.append(poet)
 
-poets = []
-for row in rows:
-    poet={
-    'Name': row[1],
-    'ID': row[0]
-    }
-    poets.append(poet)
-
-pprint(poets)
+    return poets
